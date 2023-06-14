96e03bc080bf929db81ed9d7ee456b9f041e2a22
jspl151 <jayasutha151@gmail.com>
2023-06-14 14:07:54 +0000

    Refactored

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
-const { map ,reduce} = require('@laufire/utils/collection');
+const { map, reduce } = require('@laufire/utils/collection');
 
 const data = {
   cost: 10,
   name: 'buildHouse',
   tasks: [
-      {
-          cost: 5,
-          name: 'purchase material',
-          tasks: [
-              {
-                  cost: 10,
-                  name: 'purchase cement',
-              },
-              {
-                  cost: 15,
-                  name: 'purchase steel',
-              },
-          ],
-      },
-      {              
-          cost:0,
-          name: 'invite people',
-      },
-    ],
-  }
-
-
-const getList = (data) =>
-{
-  const renderData = data.tasks ? data.tasks.map((task) => getList(task)):'-';
-  
-    return { 
-      cost : (renderData === "-")? data.cost : data.cost+reduce(renderData,(acc,cur)=>acc+cur.cost,0),
-      tasks : renderData };
-  
+    {
+      cost: 5,
+      name: 'purchase material',
+      tasks: [
+        {
+          cost: 10,
+          name: 'purchase cement',
+        },
+        {
+          cost: 15,
+          name: 'purchase steel',
+        },
+      ],
+    },
+    {
+      cost: 0,
+      name: 'invite people',
+    },
+  ],
 }
-const main = () => console.log(JSON.stringify(getList(data),null," "));
+
+const renderData = (data) => {
+  const costDetails = data.tasks ? data.tasks.map(costDetails) : '-';
+
+  return {
+    cost: (costDetails === "-") ? data.cost :
+      data.cost + reduce(costDetails, (acc, cur) => acc + cur.cost, 0),
+    tasks: costDetails
+  };
+
+}
+const main = () => console.log(JSON.stringify(renderData(data), null, " "));
 
 main();
